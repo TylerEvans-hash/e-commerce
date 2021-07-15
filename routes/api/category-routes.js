e37b92d294a56655cd0d5f2b95dcb26245392c13
Red3ffect <red3ffect117@gmail.com>
2021-07-14 21:03:02 -0700

    Adds Find One Category Route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -18,12 +18,28 @@ router.get('/', (req, res) => {
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
-    })
+    });
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+    ]
+  })
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post('/', (req, res) => {
