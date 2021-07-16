5809da0c06e003c7d9fda3ee2fcd3eb9393b4f88
Red3ffect <red3ffect117@gmail.com>
2021-07-15 20:32:18 -0700

    Adds CRUD routes to product-routes.js

--- models/index.js
+++ models/index.js
@@ -17,16 +17,32 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  as: 'product_tags',
+  //as: 'product_tag',
   foreignKey: 'product_id'
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  as: 'product_tags',
+  //as: 'product_tag',
   foreignKey: 'tag_id'
-})
+});
+
+ProductTag.belongsTo(Product, {
+  foreignKey: "product_id"
+});
+
+ProductTag.belongsTo(Tag, {
+  foreignKey: "product_id"
+});
+
+Product.hasMany(ProductTag, {
+  foreignKey: "product_id"
+});
+
+Tag.hasMany(ProductTag, {
+  foreignKey: "tag_id"
+});
 
 module.exports = {
   Product,
